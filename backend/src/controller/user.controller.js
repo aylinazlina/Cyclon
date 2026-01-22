8e8c2d7c65102d01e1a516df4c4a9cab1b5ecf89
azlinaaylin <azlinaarabi@gmail.com>
2026-01-23 00:59:29 +0600

    phoneNumber registration

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,295 +1,348 @@
-const UserModel = require('../models/user.model');
-const {apiResponse} = require('../utils/apiResponse');
-const {customError} = require('../utils/customError');
-const {asynchandeler} = require('../utils/asynchandeler');
-const {validateUser}=require('../validation/user.validation');
-const {RegistrationTemplate} = require('../template/Template');
-const {ResetPasswordEmailTemplate}=require('../template/Template');
-const {emailSend}= require('../helpers/helper');
-const crypto = require('crypto');
-
-
-
-
+const UserModel = require("../models/user.model");
+const { apiResponse } = require("../utils/apiResponse");
+const { customError } = require("../utils/customError");
+const { asynchandeler } = require("../utils/asynchandeler");
+const { validateUser } = require("../validation/user.validation");
+const { RegistrationTemplate } = require("../template/Template");
+const { ResetPasswordEmailTemplate } = require("../template/Template");
+const { emailSend } = require("../helpers/helper");
+const { smsSend } = require("../helpers/helper");
+const crypto = require("crypto");
 
 //todo:registration user
 
-exports.registration=asynchandeler(async(req,res)=>{
+exports.registration = asynchandeler(async (req, res) => {
+  const validatedData = await validateUser(req);
+  const { firstName, email, phoneNumber, password } = validatedData;
+  if (email == undefined && phoneNumber == undefined) {
+    throw new customError(401, "email/phoneNumber required!!");
+  }
 
-   const validatedData=await validateUser(req);
-   const {firstName,email,phoneNumber,password}=validatedData;
-   if(email == undefined &&  phoneNumber == undefined){
-      throw new customError(401,"email/phoneNumber required!!");
-   }
+  console.log(email);
 
-   console.log(email);
-  
-   
-   //todo:save the user inofo into database
-    const finduser=await new UserModel({
+  //todo:save the user inofo into database
+  const finduser = await new UserModel({
     firstName,
     email: email || null,
     phoneNumber: phoneNumber || null,
-    password
-   }).save();
-   
+    password,
+  }).save();
 
-   if(!finduser){
-    throw new customError(500,"user registration failed Try again later");
-   }
+  if (!finduser) {
+    throw new customError(500, "user registration failed Try again later");
+  }
 
-   //todo:random OTP generate
-   const otp= crypto.randomInt(100000,999999) ;
-   //todo:otp saving in the database
-   finduser.resetPasswordOTP = otp;
-   console.log(otp);
-   const expireTime=Date.now() + 10 * 60 * 60 * 1000;
-   if(user.email){
+  //todo:random OTP generate
+  const otp = crypto.randomInt(100000, 999999);
+  //todo:otp saving in the database
+  finduser.resetPasswordOTP = otp;
+  console.log(otp);
+  const expireTime = Date.now() + 10 * 60 * 60 * 1000;
+  if (user.email) {
+    const verifyLink = `https://form.com/verify-email/${email}`;
+    const template = RegistrationTemplate(
+      firstName,
+      verifyLink,
+      otp,
+      expireTime,
+    );
+    await emailSend(email, template);
+  }
 
-   const verifyLink=`https://form.com/verify-email/${email}`;
-   const template =RegistrationTemplate(firstName,verifyLink,otp,expireTime);
-   await emailSend(email,template);
+  //phone
 
-   }
+  if (user.phoneNumber) {
+    const verifyLink = `https://form.com/verify/${phoneNumber}`;
+    const smsbody = `hi ${user.firstName},complete your registration here: ${verifyLink}
+    This link will expre in [X hours/days].`;
+    const smsInfo = await smsSend("01753441158", smsbody);
+    if (smsInfo.response_code !== 202) {
+      console.log("SMS not send", smsInfo);
+    }
+    console.log(smsInfo);
+  }
 
-   if(user.phoneNumber){
-    const  verifyLink =`https://form.com/verify/${phoneNumber}`;
-   }
+  finduser.resetPasswordExpireTime = expireTime;
+  //todo:saving otp and expire time to database
+  await finduser.save();
 
-   finduser.resetPasswordExpireTime = expireTime;
-   //todo:saving otp and expire time to database
-   await finduser.save()
-   
-   apiResponse.sendSuccess(res,201,"Registration Successfull",{
-      // todo:postman e jeno shudhu firstName are email ta dekhai
-   firstName,email
-   });
-
-
-
-})
-
-
-//todo:login
-exports.login=asynchandeler(async(req,res)=>{
-   const validatedData=await validateUser(req);
-   const {email,phoneNumber,password}=validatedData;
-   //todo: Find the user
-   const finduser = await UserModel.findOne({$or:[{email:email},{phoneNumber:phoneNumber}]});
-   const isPasswordMatch = await finduser.compareHashPassword(password);
-
-   //todo:check if user exsists and password matches
-   if(!finduser || !isPasswordMatch){
-      throw new customError(400,"Your Password or email does not match");
-
-   }
-   // console.log(isPasswordMatch);
-
-   //todo:make an access and refresh token
-   const accessToken=await finduser.generateAccessToken();
-   const refreshToken=await finduser.generateRefreshToken();
-
-   const isProduction=process.env.NODE_ENV == "production";
-   
-   res.cookie("refreshToken",refreshToken,{
-      httpOnly: true ,
-      secure: isProduction ? true : false , //todo:http / https
-      sameSite:"lax",
-      path: "/" ,
-      maxAge: 7 * 24 * 60 *60 *1000, //todo:7days
-
-   });
-
-    console.log(accessToken,refreshToken);
-
-    //todo:now save refresh token into database
-
-
-   finduser.refreshToken=refreshToken;
-   await finduser.save();
-
-   //todo:Send success response
-   return apiResponse.sendSuccess(res, 200, "Login Successful", {
-        user: { firstName: finduser.firstName, email: finduser.email },
-        accessToken,
-        refreshToken
-    });
-  
-
-
+  apiResponse.sendSuccess(res, 201, "Registration Successfull", {
+    // todo:postman e jeno shudhu firstName are email ta dekhai
+    firstName,
+    email,
+  });
 });
 
+//todo:login
+exports.login = asynchandeler(async (req, res) => {
+  const validatedData = await validateUser(req);
+  const { email, phoneNumber, password } = validatedData;
+  //todo: Find the user
+  const finduser = await UserModel.findOne({
+    $or: [{ email: email }, { phoneNumber: phoneNumber }],
+  });
+  const isPasswordMatch = await finduser.compareHashPassword(password);
+
+  //todo:check if user exsists and password matches
+  if (!finduser || !isPasswordMatch) {
+    throw new customError(400, "Your Password or email does not match");
+  }
+  // console.log(isPasswordMatch);
+
+  //todo:make an access and refresh token
+  const accessToken = await finduser.generateAccessToken();
+  const refreshToken = await finduser.generateRefreshToken();
+
+  const isProduction = process.env.NODE_ENV == "production";
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: isProduction ? true : false, //todo:http / https
+    sameSite: "lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000, //todo:7days
+  });
+
+  console.log(accessToken, refreshToken);
+
+  //todo:now save refresh token into database
+
+  finduser.refreshToken = refreshToken;
+  await finduser.save();
+
+  //todo:Send success response
+  return apiResponse.sendSuccess(res, 200, "Login Successful", {
+    user: { firstName: finduser.firstName, email: finduser.email },
+    accessToken,
+    refreshToken,
+  });
+});
 
 //todo:email verification
-exports.emailVerification=asynchandeler(async(req,res)=>{
-   const {otp,email} = req.body;
-   console.log(otp,email);
-   if (!otp && !email){
-      throw new customError(401,"Otp or email not found");
+exports.emailVerification = asynchandeler(async (req, res) => {
+  const { otp, email } = req.body;
+  console.log(otp, email);
+  if (!otp && !email) {
+    throw new customError(401, "Otp or email not found");
+  }
 
+  if(!findUser.isEmailVerified && !findUser.isPhoneVerified){
+   if(findUser.email){
+      //send email
+      return res.status(301).redirect('');
    }
+  }
 
-   const findUser=await UserModel.findOne({
-      $and:[{email:email,},{resetPasswordOTP:otp,},{resetPasswordExpireTime:{$gt:Date.now() }}]
-   
-   
-   });
-   if(!findUser){
-      throw new customError(401,"Otp or Time expire.Try Again!!");
-   }
-   
-   //todo:after verification clearing 
+
+//   const findUser = await UserModel.findOne({
+//     $and: [
+//       { email: email },
+//       { resetPasswordOTP: otp },
+//       { resetPasswordExpireTime: { $gt: Date.now() } },
+//     ],
+//   });
+
+  const findUser= await UserModel.findOne({$or:[{email},{phoneNumber}]});
+  if(findUser.resetPasswordExpireTime > Data.now() || findUser.resetPasswordOTP == otp){
+   apiResponse.sendSucess(res,401,"Time Expires and OTP invalid",null);
+  }
+
+  //   if (findUser.resetPasswordExpireTime > Data.now()) {
+
+  //       const randomNumber = crypto.randomInt(100000, 999999);
+  //       const expireTime = Data.now() + 10 * 60 * 60 * 1000;
+
+  //     if (findUser.email) {
+  //       //send email
+
+  //       const verifyLink = `https://forn.com/verify/${findUser.email}`;
+  //       const template = RegistrationTemplate(
+  //         firstName,
+  //         verifyLink,
+  //         randomNumber,
+  //         expireTime,
+  //       );
+
+  //     await emailSend(email, template);
+  //     return res.status(301).redirect(verifyLink);
+  //   }else{
+
+  //     const verifyLink = `https://form.com/verify/${phoneNumber}`;
+  //     const smsbody = `hi ${findUser.firstName},complete your registration here: ${verifyLink}
+  //     This link will expre in [X hours/days].`;
+  //     const smsInfo = await smsSend(phoneNumber, smsbody);
+  //     if (smsInfo.response_code !== 202) {
+  //       console.log("SMS not send", smsInfo);
+  //     }
+
+  //   }
+  //   findUser.resetPasswordExpireTime = expireTime;
+  //   findUser.resetPasswordOTP =randomNumber;
+  //   await findUser.save()
+  //   apiResponse.sendSuccess(res,401,"Check Your email/phone",findUser)
+  // }
+
+  //   if (!findUser.email == null) {
+  //     //again send sms
+  //     return res
+  //       .status(301)
+  //       .redirect(`www.fn.com/verify/${findUser.phoneNumber}`);
+  //   } else {
+  //     //again send email
+  //     return res.status(301).redirect(`www.fn.com/verify/${findUser.email}`);
+  //   }
+
+  if (!findUser) {
+    throw new customError(401, "Otp or Time expire.Try Again!!");
+  }
+
+  //todo:after verification clearing
+  if(findUser.email){
    findUser.resetPasswordOTP = null;
-   findUser.resetPasswordExpireTime = null;
-   findUser.isEmailVerified = true;
+  findUser.resetPasswordExpireTime = null;
+  findUser.isEmailVerified = true;
+  }else{
+   findUser.resetPasswordOTP = null;
+  findUser.resetPasswordExpireTime = null;
+  findUser.isEmailVerified = true;
+  }
+  await findUser.save()
 
-   apiResponse.sendSuccess(res,200,"Email verified successfully",{
-      email:findUser.email,
-      firstName:findUser.firstName,
-   });
-   console.log(findUser);
-})
 
+
+  apiResponse.sendSuccess(res, 200, "Email verified successfully", {
+    email: findUser.email,
+    firstName: findUser.firstName,
+  });
+  console.log(findUser);
+});
 
 //todo:forgot password
-exports.forgetPassword=asynchandeler(async(req,res)=>{
+exports.forgetPassword = asynchandeler(async (req, res) => {
+  const { firstName, email } = req.body;
 
-  const {firstName,email}=req.body;
-
-  if(!email){
-   throw new customError(401,"eamil missing");
+  if (!email) {
+    throw new customError(401, "eamil missing");
   }
- const finduser=await UserModel.findOne({email:email});
+  const finduser = await UserModel.findOne({ email: email });
 
- if(!finduser){
-   throw new customError(401,"user not found");
- }
+  if (!finduser) {
+    throw new customError(401, "user not found");
+  }
 
- //todo:random OTP generate
- const otp = crypto.randomInt(100000,999999);
- const expireTime = Date.now() + 10 * 60 * 60 * 1000;
- const verifyLink ='http://form.com/resetpassoword/${email}';
- const template = ResetPasswordEmailTemplate(firstName,verifyLink,otp,expireTime);
- await emailSend(email,template);
+  //todo:random OTP generate
+  const otp = crypto.randomInt(100000, 999999);
+  const expireTime = Date.now() + 10 * 60 * 60 * 1000;
+  const verifyLink = "http://form.com/resetpassoword/${email}";
+  const template = ResetPasswordEmailTemplate(
+    firstName,
+    verifyLink,
+    otp,
+    expireTime,
+  );
+  await emailSend(email, template);
 
- apiResponse.sendSuccess(res,301,"check your email",null);
-
-
-
-})
-
+  apiResponse.sendSuccess(res, 301, "check your email", null);
+});
 
 //todo:reset password
 
-exports.resetPassword=asynchandeler(async(req,res)=>{
-   const {email,newPassword,confirmPassword}=req.body;
-   if(!email){
-      throw new customError(401,"email missing");
-   }
-   if(!newPassword){
-      throw new customError(401,"new password missing");
-   }
-   if(!confirmPassword){
-      throw new customError(401,"confirm password missing");
-   }
-   if(newPassword !== confirmPassword ){
-      throw new customError(401,"newPassword or confirm passowrd does not match");
-   }
+exports.resetPassword = asynchandeler(async (req, res) => {
+  const { email, newPassword, confirmPassword } = req.body;
+  if (!email) {
+    throw new customError(401, "email missing");
+  }
+  if (!newPassword) {
+    throw new customError(401, "new password missing");
+  }
+  if (!confirmPassword) {
+    throw new customError(401, "confirm password missing");
+  }
+  if (newPassword !== confirmPassword) {
+    throw new customError(
+      401,
+      "newPassword or confirm passowrd does not match",
+    );
+  }
 
-   //todo: find the user
+  //todo: find the user
 
-   const finduser= await UserModel.findOne({email});
-   if(!finduser){
-      throw new customError(401,"user not found ");
-   }
+  const finduser = await UserModel.findOne({ email });
+  if (!finduser) {
+    throw new customError(401, "user not found ");
+  }
 
-   finduser.password == newPassword;
-   finduser.resetPasswordOTP  = null;
-   finduser.resetPasswordExpireTime = null;
+  finduser.password == newPassword;
+  finduser.resetPasswordOTP = null;
+  finduser.resetPasswordExpireTime = null;
 
-   await finduser.save();
-   
-   apiResponse.sendSuccess(res,200,"password reset successfull",finduser);
+  await finduser.save();
 
-
-
-})
-
-//todo:logout user
-exports.logoutuser=asynchandeler(async(req,res)=>{
-
-   const findUser=await UserModel.findById(req.user.email);
-   console.log(req.user);
-   if(!findUser){
-      throw new customError(401,"User not found");
-   }
-
-//   if(!refreshToken){
-//    throw new customError(401,"refresh token missing");
-//   }
-
-  //todo:clear the cookies
-  res.clearCookie("refreshToken" , {
-   httpOnly: true,
-   secure:isProduction ? true : false,
-   samSite: "none" ,
-   path: "/", //This must match the path used when setting the cookie
-  }) ;
-
-  //todo:find the user
-  findUser.refreshToken = null ;
-  await findUser.save() ;
-  apiResponse.sendSuccess(res,200,"Logout Successfull", findUser);
-
-
-
-
+  apiResponse.sendSuccess(res, 200, "password reset successfull", finduser);
 });
 
+//todo:logout user
+exports.logoutuser = asynchandeler(async (req, res) => {
+  const findUser = await UserModel.findById(req.user.email);
+  console.log(req.user);
+  if (!findUser) {
+    throw new customError(401, "User not found");
+  }
+
+  //   if(!refreshToken){
+  //    throw new customError(401,"refresh token missing");
+  //   }
+
+  //todo:clear the cookies
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: isProduction ? true : false,
+    samSite: "none",
+    path: "/", //This must match the path used when setting the cookie
+  });
+
+  //todo:find the user
+  findUser.refreshToken = null;
+  await findUser.save();
+  apiResponse.sendSuccess(res, 200, "Logout Successfull", findUser);
+});
 
 //todo:get me controller
 
-exports.getMe=asynchandeler(async(req,res)=>{
+exports.getMe = asynchandeler(async (req, res) => {
+  const id = req.user.id;
+  const findUser = await UserModel.findById(id);
+  if (!findUser) {
+    throw new cutomError(401, "User not found");
+  }
 
-    const id=req.user.id;
-    const findUser=await UserModel.findById(id);
-    if(!findUser){
-      throw new cutomError(401,"User not found");
-    }
-
-   apiResponse.sendSuccess(res,200,"User found successfully",findUser);
-
-
-})
-
+  apiResponse.sendSuccess(res, 200, "User found successfully", findUser);
+});
 
 //todo:get refresh token
 exports.getrefreshToken = asynchandeler(async (req, res) => {
-    // 1. Get the token and clean it up
-    // .trim() is important because your .replace("...", " ") adds a space
-    const token = req.headers.cookie?.replace("refreshToken=", "").trim();
+  // 1. Get the token and clean it up
+  // .trim() is important because your .replace("...", " ") adds a space
+  const token = req.headers.cookie?.replace("refreshToken=", "").trim();
 
-    if (!token) {
-        throw new customError(401, "Token not found!");
-    }
+  if (!token) {
+    throw new customError(401, "Token not found!");
+  }
 
-    // 2. Find the user
-    const findUser = await UserModel.findOne({ refreshToken: token });
+  // 2. Find the user
+  const findUser = await UserModel.findOne({ refreshToken: token });
 
-    // 3. Validation Check (The missing piece)
-    if (!findUser) {
-        throw new customError(403, "Invalid refresh token or user not found.");
-    }
+  // 3. Validation Check (The missing piece)
+  if (!findUser) {
+    throw new customError(403, "Invalid refresh token or user not found.");
+  }
 
-    // 4. Generate the new token
-    const accessToken = findUser.generateAccessToken();
+  // 4. Generate the new token
+  const accessToken = findUser.generateAccessToken();
 
-    apiResponse.sendSuccess(res, 200, "Access token refreshed successfully.", {
-        accessToken: accessToken,
-        userName: findUser.firstName,
-        email: findUser.email,
-    });
+  apiResponse.sendSuccess(res, 200, "Access token refreshed successfully.", {
+    accessToken: accessToken,
+    userName: findUser.firstName,
+    email: findUser.email,
+  });
 });
-
-
-
--- backend/src/helpers/helper.js
+++ backend/src/helpers/helper.js
@@ -30,4 +30,29 @@ exports.emailSend=async (email,template) => {
 };
 
 
+//todo:SMS sender
+exports.smsSend= async(phoneNumber,message)=>{
+
+  try{
+    const response= await axios.post(process.env.API_URL,{
+      api_key:process.env.API_KEY ,
+      senderid:process.env.SENDER_ID,
+      number:Array.isArray(phoneNumber) ? phoneNumber.join(','):phoneNumber,
+      message:"hello from mern 2403"
+    })
+
+    return response.data;
+  
+  }catch(error){
+    console.log("error from send sms" ,error);
+    throw new customError(500,"smsSend function"+ error);
+
+  }
+
+
+
+}
+
+
+
 
